d93f986f9cb9ea5f51e2692515bab16f6e13b854
Zachary Kunzer <zjkunzer@gmail.com>
2024-10-24 15:00:54 -0500

    Setting up Java framework
    Sets up Java Framework and required classes for Zach

---
+++ it326/src/Account.java
@@ -0,0 +1,11 @@
+//Acount Class
+public class Account{
+    String username;
+    int phoneNumber;
+    int uid;
+    public Account(String username, int phoneNumber, int uid){
+        this.username=username;
+        this.phoneNumber=phoneNumber;
+        this.uid=uid;
+    }
+}
