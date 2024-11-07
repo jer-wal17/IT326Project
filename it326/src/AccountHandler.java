15d6deb0ed8c56a548b7b7b0ab4753ea2d4b682a
Zachary Kunzer <zjkunzer@gmail.com>
2024-11-07 17:20:44 -0600

    Basic Class Set Up
    Just basic placeholder classes for the classes

--- it326/src/AccountHandler.java
+++ it326/src/AccountHandler.java
@@ -1,3 +1,15 @@
 public class AccountHandler{
+    public AccountHandler(){
+
+    }
+    public boolean validateLoginRequest(){
+        return true;
+    }
+    public boolean validateLogoutRequest(){
+        return true;
+    }
+    public boolean validateEditAccountRequest(){
+        return true;
+    }
     
 }
--- it326/src/AccountManager.java
+++ it326/src/AccountManager.java
@@ -1,3 +1,23 @@
 public class AccountManager{
-    
+    Account curAccount;
+
+    public AccountManager(Account userAccount){
+        this.curAccount = userAccount;
+    }
+
+    public boolean editAccount(){
+        return true;
+    }
+    public boolean deleteAccount(){
+        return true;
+    }
+    public Account createAccount(String username, String password, int phoneNumber){
+        return new Account(username, phoneNumber, 0000);
+    }
+    public Account logIn(String username, String password){
+        return new Account(username, 0000,0000);
+    }
+    public boolean logOut(){
+        return true;
+    }
 }
