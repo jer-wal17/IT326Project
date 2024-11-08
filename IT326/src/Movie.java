8b32fedd1e37b53d4daa7b2656dfd6c0a13022c6
JD Waldron <jeromew1714@gmail.com>
2024-11-07 18:02:13 -0600

    Create skeletons of API classes

---
+++ IT326/src/Movie.java
@@ -0,0 +1,9 @@
+public class Movie {
+    String movieName;
+    int movieYear;
+
+    public Movie(String name, int year) {
+        this.movieName = name;
+        this.movieYear = year;
+    }
+}
