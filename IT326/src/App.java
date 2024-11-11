3eb9711613bf8bf9c34c2eb06f24f2306ad4775c
Alex Lerch <106268256+alex-lerch@users.noreply.github.com>
2024-11-11 08:05:57 -0600

    functionality for creating/reading/updating/deleting Account type objects from the database has been implemented

--- IT326/src/App.java
+++ IT326/src/App.java
@@ -6,18 +6,7 @@ public class App
     {
 
         System.out.println("Hello, World!");
- 
-        // connect to database
-        MySQLConnectionSerializer connector = new MySQLConnectionSerializer();
-        connector.connect();
 
-        // query the database and print results
-        QueryTester qt = new QueryTester();
-        String accountToPrint = "alexlerch";
-        qt.retrieveAccount(accountToPrint, connector.getConn());
-
-        // close the database
-        connector.disconnect();
 
     }
 }
