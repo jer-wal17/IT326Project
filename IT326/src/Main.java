e58e63b6176756db17a7274b0e2b1c0cacac68d7
akinsiku98 <166594109+akinsiku98@users.noreply.github.com>
2024-11-14 14:58:33 -0600

    Groupjava

---
+++ IT326/src/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+    public static void main(String[] args) {
+        // Main application logic here
+    }
+    public void logIn() {
+            // Implementation code here
+        }
+    
+    public void logOut() {
+            // Implementation code here
+        }
+    
+    public void findGroup() {
+            // Implementation code here
+        }
+    
+    public void editAccount() {
+            // Implementation code here
+        }
+    
+    public void leaveGroup() {
+            // Implementation code here
+        }
+ 
+}
+
