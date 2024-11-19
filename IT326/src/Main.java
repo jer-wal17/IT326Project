ed9d551767ad248ba4fe2a1d9dc266957b837150
akinsiku98 <166594109+akinsiku98@users.noreply.github.com>
2024-11-19 15:05:05 -0600

    Update Main.java

--- IT326/src/Main.java
+++ IT326/src/Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         // Main application logic here
+        System.out.println("Hello World");
     }
     public void logIn() {
             // Implementation code here
