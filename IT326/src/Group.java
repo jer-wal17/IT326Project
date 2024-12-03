d8ec19ba063d2187ac8b919f6effa6d71309ac2c
akinsiku98 <166594109+akinsiku98@users.noreply.github.com>
2024-12-03 14:17:55 -0600

    Updated Version

--- IT326/src/Group.java
+++ IT326/src/Group.java
@@ -1,21 +1,27 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Group {
-  int maxSize;
-  int groupID;
-  String movie;
-  String meetingAddress;
-  List<Account> members;
-  LocalDate meetingDate;
+    private int groupID;
+    private Movie movie;
+    private String meetingAddress;
+    private LocalDate meetingDate;
+    private Account owner; // Owner of the group
+    private int maxSize; // Maximum size of the group
+    private List<Account> members; // Members of the group
   
-  public Group(int groupID, String movie, List<Account> members, String meetingAddress, LocalDate meetingDate) {
-    this.groupID = groupID;
-    this.movie = movie;
-    this.members = members;
-    this.meetingAddress = meetingAddress;
-    this.meetingDate = meetingDate;
-}
+  public Group(int groupID, Movie movie, String meetingAddress, LocalDate meetingDate, Account owner, int maxSize) {
+        this.groupID = groupID;
+        this.movie = movie;
+        this.meetingAddress = meetingAddress;
+        this.meetingDate = meetingDate;
+        this.owner = owner;
+        this.maxSize = maxSize;
+        this.members = new ArrayList<>();
+        this.members.add(owner); // Add the owner as the first member
+    }
+// Getters and setters
 public int getGroupID() {
     return groupID;
 }
@@ -24,20 +30,12 @@ public void setGroupID(int groupID) {
     this.groupID = groupID;
 }
 
-public String getMovie() {
+public Movie getMovie() {
     return movie;
 }
 
-public void setMovie(String movie) {
+public void setMovie(Movie movie) {
     this.movie = movie;
-}
-
-public List<Account> getMembers() {
-    return members;
-}
-
-public void setMembers(List<Account> members) {
-    this.members = members;
 }
 
 public String getMeetingAddress() {
@@ -55,7 +53,27 @@ public LocalDate getMeetingDate() {
 public void setMeetingDate(LocalDate meetingDate) {
     this.meetingDate = meetingDate;
 }
+
+public Account getOwner() {
+    return owner;
 }
+
+public void setOwner(Account owner) {
+    this.owner = owner;
+}
+
+public int getMaxSize() {
+    return maxSize;
+}
+
+public void setMaxSize(int maxSize) {
+    this.maxSize = maxSize;
+}
+
+public List<Account> getMembers() {
+    return members;
+}
+    }
 
 
   
