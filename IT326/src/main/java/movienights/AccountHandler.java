90bb10688f875ee1e66890b62761f33eb97b0b56
Zachary Kunzer <zjkunzer@gmail.com>
2024-12-06 18:50:58 -0600

    12/6/2024 6PM update
    Account portion of controller is done.
    AccountHandler is finished
    Methods for the Group portion of controller are create but substance might need to change.
    AccountHandler has basic functionality but data validation still needs to be added

--- IT326/src/main/java/movienights/AccountHandler.java
+++ IT326/src/main/java/movienights/AccountHandler.java
@@ -7,17 +7,28 @@ public class AccountHandler{
     public AccountHandler(){
 
     }
+
+    public Account validteCreateAccount(String username, String password, String phoneNumber, int uid) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+        //Validate data given by user here
+        return manager.createAccount(username, password, phoneNumber, uid);
+    }
     public Account validateLoginRequest(int uid, String password)  throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+        //Validate data given by user here
         return manager.logIn(uid, password);
     }
     public boolean validateLogoutRequest() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
         return manager.logOut();
     }
-    public Account validateEditAccountRequest() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
-        return manager.editAccount();
+    public boolean validateEditAccountRequest(Account changeTo) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+        //Validate data given by user here
+        return true;
     }
-    public Account validteCreateAccount(String username, String password, String phoneNumber, int uid) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
-        return manager.createAccount(username, password, phoneNumber, uid);
+    public boolean validateChangePrefrencesRequest() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+        //Validate data given by user here
+        return true;
+    }
+    public boolean validateDeleteAccountRequest() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+        return manager.deleteAccount();
     }
     
 }
